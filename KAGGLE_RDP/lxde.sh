4a857a2a63232b95793d92aa33e6d54ad5014676
TaheemReal <132823975+TaheemReal@users.noreply.github.com>
2023-09-01 20:00:51 +0530

    Add files via upload

--- KAGGLE_RDP/lxde.sh
+++ KAGGLE_RDP/lxde.sh
@@ -1,6 +1,10 @@
-./ngrok tcp 5901 &
+#!/bin/bash
+
+# Start QEMU
+qemu-system-x86_64 -accel tcg -smp cores=8 -m 8G -vnc :0 -cdrom Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO -boot d -hda disk.img &
+
+# Wait for a brief moment to allow QEMU to start
 sleep 10
-Xvfb :1 -screen 0 1024x768x16 &
-export DISPLAY=:1
-startlxde
-killall Xvfb
+
+# Start ngrok for VNC tunnel
+./ngrok tcp 5900
