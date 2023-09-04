101a62f78db25371f7af37b920f579c77c2a8bf3
TaheemReal <132823975+TaheemReal@users.noreply.github.com>
2023-09-04 21:02:14 +0530

    Update lxde.sh

--- KAGGLE_RDP/lxde.sh
+++ KAGGLE_RDP/lxde.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-# Start QEMU
-qemu-system-x86_64 -accel tcg -smp cores=8 -m 8G -vnc :0 -cdrom Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO -boot d -hda disk.img &
-
-# Wait for a brief moment to allow QEMU to start
-sleep 10
-
-# Start ngrok for VNC tunnel
-./ngrok tcp 5900
+startlxde &
+./ngrok tcp 5901
